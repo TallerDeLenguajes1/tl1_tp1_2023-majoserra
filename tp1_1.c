3f422019ad6cba0ad92032777aa847ab276996d3
Majo Serra <mariajoseserra03@gmail.com>
2023-03-26 23:34:53 -0300

    Modificacion con puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hola Mundo");
+    int *puntero, num;
+
+    num = 2;
+    puntero = &num;
+
+    printf("Contenido del puntero: %i", *puntero);
+    printf("Dirección de memoria almacenada por el puntero: %i", puntero);
+    printf("Dirección de memoria de la variable: %i", puntero);
+    printf("Dirección de memoria del puntero: %i", &puntero);
+    
     return 0;
 }
