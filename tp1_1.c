6e565dceda3a7f894551f316c8fa499d1ceff99c
Majo Serra <mariajoseserra03@gmail.com>
2023-03-26 23:38:11 -0300

    modificacion

--- tp1_1.c
+++ tp1_1.c
@@ -6,10 +6,10 @@ int main(){
     num = 2;
     puntero = &num;
 
-    printf("Contenido del puntero: %i", *puntero);
-    printf("Dirección de memoria almacenada por el puntero: %i", puntero);
-    printf("Dirección de memoria de la variable: %i", puntero);
-    printf("Dirección de memoria del puntero: %i", &puntero);
+    printf("\nContenido del puntero: %i", *puntero);
+    printf("\nDirección de memoria almacenada por el puntero: %i", puntero);
+    printf("\nDirección de memoria de la variable: %i", puntero);
+    printf("\nDirección de memoria del puntero: %i", &puntero);
     
     return 0;
 }
